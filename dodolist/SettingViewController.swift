98ecd217b1546a2309841a5c5bb6e272f39edac8
201411108 <aggsae@gmail.com>
2020-04-11 17:58:32 +0900

    Implement Setting done button alert
    add UIAlertController in SettingViewController.swift.

--- dodolist/SettingViewController.swift
+++ dodolist/SettingViewController.swift
@@ -16,9 +16,18 @@ class SettingViewController : UIViewController {
     
     // 변경된 저장 사항을 저장하고 다시 원래 화면으로 돌아감.
     @IBAction func done(_ sender: Any) {
-        // TODO ::
-        // 저장된 사항을 확정할 것인지 별도의 화면 띄우기
-        // 어떻게 구현할 것인지 방법 찾아봐야함.
+
+        // alert 창, 각 버튼 설정
+        let alert = UIAlertController(title: "확인", message: "설정을 저장하시겠습니까?", preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        let ok = UIAlertAction(title: "확인", style: .default) { (_) in
+        // TODO :: 저장 내용을 변경해서 반영하는 코드 추가
         self.presentingViewController?.dismiss(animated: true)
+
+        }
+
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        self.present(alert, animated: false)
     }
 }
