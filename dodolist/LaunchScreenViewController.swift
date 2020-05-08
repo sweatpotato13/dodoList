92c92dca995f16631407b50e68233248d3990c73
Handong Kim <aggsae@gmail.com>
2020-05-08 22:52:30 +0900

    Implement lock mode
    잠금 모드 구현, 최초 인식 가능
    취소 버튼 액션 해답 찾지 못함

---
+++ dodolist/LaunchScreenViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LaunchScreenViewController.swift
+//  dodolist
+//
+//  Created by handongkim on 2020/05/03.
+//  Copyright © 2020 dodo. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+    
+}
